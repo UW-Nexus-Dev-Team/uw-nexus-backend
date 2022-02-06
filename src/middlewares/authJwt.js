26ccbe6c87606c493f6650553c916b207b06af2d
Daniel Pham <danielpham0@gmail.com>
2022-02-05 23:04:43 -0800

    slight adjustment to make JWT into cookies

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -19,27 +19,6 @@ authJwt = (req,res,next) => {
     }
 }
 
-// authJwt = (req, res, next) => {
-//     const token = req.headers["x-access-token"];
-//     const authHeader = req.headers.authorization;
-
-//     if (authHeader) {
-//         const token = authHeader.split(' ')[1];
-
-//         jwt.verify(token, config.JWT_SECRET, (err, user) => {
-//             if (err) {
-//                 res.status(401).send({ message: "Unauthorized!" });
-//                 return;
-//             }
-//             req.id = user.id;
-//             next();
-//         });
-//     } else {
-//         res.status(403).send({ message: "No token provided!" });
-//         return;
-//     }
-// };
-
 // isAdmin = (req, res, next) => {
 //   User.findById(req.userId).exec((err, user) => {
 //     if (err) {
