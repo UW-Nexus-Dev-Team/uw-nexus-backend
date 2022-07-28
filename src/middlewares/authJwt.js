e181ceaad4d992d4084c0a2bcab2df37e5b6b856
gboksm11 <gboksm11@uw.edu>
2022-07-28 07:39:31 -0700

    reposition console log

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -3,8 +3,8 @@ const config = require("../config/index.js");
 
 authJwt = (req,res,next) => {
     const { accessToken } = req.cookies;
+    console.log(`Received access token = ${accessToken}`);
     if (accessToken) {
-        console.log(`Received access token = ${accessToken}`)
         jwt.verify(accessToken, config.JWT_SECRET, (err, user) => {
             if (err) {
                 res.status(401).send({ message: "Unauthorized!" });
